72b7ed7901eb7dc9f4abdbe61467d2c27dca60c7
Grimmer Kang <grimmer0125@gmail.com>
2014-07-19 10:11:34 +0800

    Add missing file.

---
+++ Sample/Constants.h
@@ -0,0 +1,27 @@
+//
+//  Constants.h
+//  libPusher
+//
+//  Created by Grimmer on 2014/7/17.
+//
+//
+
+#ifndef libPusher_Constants_h
+#define libPusher_Constants_h
+
+
+
+#define PUSHER_API_KEY @"b6ad97ea51c01c300adc"
+#define PUSHER_APP_ID @"81903"
+#define PUSHER_API_SECRET @"7da4eacdc53409262803"
+
+
+// run rake authserver:start to run
+//#define PRESENCE_AUTH_URL @"http://localhost:9292/presence/auth"
+
+// credentials for private/presence channels
+#define CHANNEL_AUTH_USERNAME @"admin"
+#define CHANNEL_AUTH_PASSWORD @"letmein"
+
+
+#endif
